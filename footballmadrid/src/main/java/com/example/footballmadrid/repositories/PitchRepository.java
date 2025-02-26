1e48d2706145d02b1ce997cf77922ed31b477308
zZAiron <163293416+zZAiron@users.noreply.github.com>
2025-02-26 14:34:51 +0100

    Delete footballmadrid directory

--- footballmadrid/src/main/java/com/example/footballmadrid/repositories/PitchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.footballmadrid.repositories;
-
-import com.example.footballmadrid.models.PitchModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PitchRepository extends JpaRepository<PitchModel, Long> {
-}
--- footballmadrid/src/main/java/com/example/footballmadrid/repositories/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.footballmadrid.repositories;
-
-import com.example.footballmadrid.models.UsersModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<UsersModel, Long> {
-}
